e265f0d1660fb07e0dd2099e32a5a2a6c1f71ea3
kavish <kavishwer2301@gmail.com>
2026-01-22 14:39:38 +0800

    Feat: Rename Duke to Patrick

---
+++ AI.md
@@ -0,0 +1,2 @@
+1. Generated ASCII art logo for "Patrick" to replace the original "Duke" logo in Patrick.java
+2. Updated EXPECTED.TXT to match the new Patrick ASCII art logo
---
+++ src/main/java/Patrick.java
@@ -0,0 +1,10 @@
+public class Patrick {
+    public static void main(String[] args) {
+        String logo = " ____       _        _      _    \n"
+                + "|  _ \\ __ _| |_ _ __(_) ___| | __\n"
+                + "| |_) / _` | __| '__| |/ __| |/ /\n"
+                + "|  __/ (_| | |_| |  | | (__|   < \n"
+                + "|_|   \\__,_|\\__|_|  |_|\\___|_|\\_\\\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
