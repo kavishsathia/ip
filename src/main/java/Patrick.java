98b96e1eb4a7a9eb9403652cbf0c8fda187b3d03
kavish <kavishwer2301@gmail.com>
2026-01-22 14:53:12 +0800

    Feat: Add Echo

--- src/main/java/Patrick.java
+++ src/main/java/Patrick.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
 public class Patrick {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____       _        _      _    \n"
                 + "|  _ \\ __ _| |_ _ __(_) ___| | __\n"
                 + "| |_) / _` | __| '__| |/ __| |/ /\n"
                 + "|  __/ (_| | |_| |  | | (__|   < \n"
                 + "|_|   \\__,_|\\__|_|  |_|\\___|_|\\_\\\n";
         System.out.println("Assistant: Hello from\n" + logo);
-        System.out.println("What can I do for you?");
+        System.out.print("What can I do for you?\nUser: ");
 
-        System.out.println("Assistant: Bye. Hope to see you again soon!");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Assistant: Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.print("Assistant: " + input + "\nUser: ");
+        }
     }
 }
